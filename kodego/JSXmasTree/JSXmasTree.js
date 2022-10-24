9fb86bc200af29705d6ec35a6f93cd1e123aacb0
natarake <30865043+natarake@users.noreply.github.com>
2022-10-24 16:06:13 +0800

    Update JSXmasTree.js

--- kodego/JSXmasTree/JSXmasTree.js
+++ kodego/JSXmasTree/JSXmasTree.js
@@ -1,26 +1,20 @@
-let height = prompt("Please enter a number between 20 and 24 to make a xmasTree");
+let height = prompt(
+  "Please enter a number between 20 and 24 to make a xmasTree"
+);
 const star = " &nbsp*&nbsp ";
 
 for (let i = 0; i < height; i++) {
   document.write(`<p class="xmasTree"> ${star.repeat(i + 1)} </p>`);
 }
 
-// const colorGen = () => {
-//   let xmasTree = document.querySelector(".xmasTree");
-//   let flashText = document.getElementById("flashText");
-//   const red = Math.floor(Math.random() * 256);
-//   const green = Math.floor(Math.random() * 256);
-//   const blue = Math.floor(Math.random() * 256);
-//   console.log(`rgb(${red},${green},${blue})`);
-//   xmasTree.style.color = `rgb(${red},${green},${blue})`;
-//   flashText.style.color = `rgb(${red},${green},${blue})`;
-// };
-
 const colorGenTree = () => {
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
   // console.log(`rgb(${red},${green},${blue})`);
+  document.getElementById(
+    "rgbValuesTree"
+  ).innerHTML = `Xmas Tree = rgb(${red},${green},${blue})`;
   let xmasTree = document.querySelectorAll(".xmasTree");
   let index = 0;
   let length = xmasTree.length;
@@ -34,18 +28,31 @@ const colorGenText = () => {
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
   const blue = Math.floor(Math.random() * 256);
+  document.getElementById(
+    "rgbValuesText"
+  ).innerHTML = `Merry Xmas = rgb(${red},${green},${blue})`;
+
   let flashText = document.getElementById("flashText");
   flashText.style.color = `rgb(${red},${green},${blue})`;
 };
 
-function startTreeInterval() {
-  startColor = setInterval(colorGenTree, 700);
-}
-
-function startTextInterval() {
-  startColor = setInterval(colorGenText, 700);
+let treeInterval = prompt("Input a number divisible by 100 for Tree Interval");
+let textInterval = prompt("Input a number divisible by 100 for Text Interval");
+function startInterval() {
+  startColor = setInterval(colorGenTree, `${treeInterval}`);
+  startColor2 = setInterval(colorGenText, `${textInterval}`);
 }
 
 function stopInterval() {
   clearInterval(startColor);
+  clearInterval(startColor2);
+}
+
+// setTimeout(function stopInterval() {
+//   clearInterval(startColor);
+//   clearInterval(startColor2);
+// }, 5000)
+
+function resetFlash() {
+  window.location.reload();
 }
