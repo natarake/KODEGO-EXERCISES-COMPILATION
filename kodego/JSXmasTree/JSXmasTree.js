64cd2bac0f138f304dae8cf9d692d2ea48e23ee5
natarake <30865043+natarake@users.noreply.github.com>
2022-10-24 18:47:42 +0800

    Update JSXmasTree.js

--- kodego/JSXmasTree/JSXmasTree.js
+++ kodego/JSXmasTree/JSXmasTree.js
@@ -3,15 +3,14 @@ let height = prompt(
 );
 let stand = height / 4;
 const star = " |*| ";
-const baseStand = "||||||||"
+const baseStand = "||||||||||||";
 
 for (let i = 0; i < height; i++) {
   document.write(`<p class="xmasTree"> ${star.repeat(i + 1)} </p>`);
 }
-for (let a = 0; a < stand; a++) { 
+for (let a = 0; a < stand; a++) {
   document.write(`<p class="xmasTree"> ${baseStand.repeat(a + 1)} </p>`);
 }
-
 
 const colorGenTree = () => {
   const red = Math.floor(Math.random() * 256);
@@ -42,24 +41,45 @@ const colorGenText = () => {
   flashText.style.color = `rgb(${red},${green},${blue})`;
 };
 
+const rgbValuesTextColor = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  let rgbValuesTextTextColor = document.getElementById("rgbValuesText");
+  rgbValuesTextTextColor.style.color = `rgb(${red},${green},${blue})`;
+};
+
+const rgbValuesTreeColor = () => {
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
+  let rgbValuesTreeTextColor = document.getElementById("rgbValuesTree");
+  rgbValuesTreeTextColor.style.color = `rgb(${red},${green},${blue})`;
+};
+
 let treeInterval = prompt("Input a number divisible by 100 for Tree Interval");
 let textInterval = prompt("Input a number divisible by 100 for Text Interval");
 const startInterval = () => {
   startColor = setInterval(colorGenTree, `${treeInterval}`);
   startColor2 = setInterval(colorGenText, `${textInterval}`);
+  startColor3 = setInterval(rgbValuesTextColor, `${textInterval}`);
+  startColor4 = setInterval(rgbValuesTreeColor, `${textInterval}`);
+  document.body.style.backgroundColor = "#2b3036";
   const starPosition = () => {
     if (height >= 1) {
-      let starPos = document.getElementById('star');
+      let starPos = document.getElementById("star");
       starPos.style.top = "80px";
     }
-  }
+  };
   starPosition();
-}
+};
 
 const stopInterval = () => {
   clearInterval(startColor);
   clearInterval(startColor2);
-}
+  clearInterval(startColor3);
+  clearInterval(startColor4);
+};
 
 // setTimeout(function stopInterval() {
 //   clearInterval(startColor);
@@ -68,4 +88,4 @@ const stopInterval = () => {
 
 const resetFlash = () => {
   window.location.reload();
-}
+};
